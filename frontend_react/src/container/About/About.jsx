086b183cfae3c283a4b3d0edd525b957db2dfb44
Gregdabro <gregmonak@gmail.com>
2022-08-06 14:44:37 +0200

    implement about section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,8 +1,58 @@
 import React from "react";
 import "./About.scss";
+import { motion } from "framer-motion";
+import  { images } from "../../constants";
+
 const About = () => {
+
+    const about = [
+        {
+            title: "Web Development",
+            description: "I am a good developer",
+            imgUrl: images.about01
+        },
+        {
+            title: "Frontend Development",
+            description: "I am a good developer",
+            imgUrl: images.about02
+        },
+        {
+            title: "Backend Development",
+            description: "I am a good developer",
+            imgUrl: images.about03
+        },
+        {
+            title: "MERN Stack",
+            description: "I am a good developer",
+            imgUrl: images.about04
+        }
+    ]
+
     return (
-        <h1>About</h1>
+        <>
+            <h2 className="head-text">I Know That <span>Good Dev</span> <br/> means <span>Good Business</span>
+            </h2>
+
+            <div className="app__profiles">
+                {about.map((about, index) => (
+                    <motion.div
+                        whileInView={{ opacity: 1 }}
+                        whileHover={{ scale: 1.1 }}
+                        transition={{ duration: 0.5, type: "tween"}}
+                        className="app__profile-item"
+                        key={about.title + index}
+                    >
+                        <img src={about.imgUrl} alt={about.title} />
+                        <h2 className="bold-text" style={{margin: 20}}>
+                            {about.title}
+                        </h2>
+                        <p className="p-text" style={{margin: 10}}>
+                            {about.description}
+                        </p>
+                    </motion.div>
+                ))}
+            </div>
+        </>
     );
 };
 
