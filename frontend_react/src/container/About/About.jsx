1971cb3c4c88fe494460753f543a084416291f2c
Gregdabro <gregmonak@gmail.com>
2022-08-07 11:38:20 +0200

    implement about sanity data

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,32 +1,18 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./About.scss";
 import { motion } from "framer-motion";
 import  { images } from "../../constants";
+import { urlFor, client } from "../../client";
 
 const About = () => {
 
-    const about = [
-        {
-            title: "Web Development",
-            description: "I am a good developer",
-            imgUrl: images.about01
-        },
-        {
-            title: "Frontend Development",
-            description: "I am a good developer",
-            imgUrl: images.about02
-        },
-        {
-            title: "Backend Development",
-            description: "I am a good developer",
-            imgUrl: images.about03
-        },
-        {
-            title: "MERN Stack",
-            description: "I am a good developer",
-            imgUrl: images.about04
-        }
-    ]
+    const [abouts, setAbouts] = useState([]);
+
+    useEffect(() => {
+        const query = '*[_type == "abouts"]';
+        client.fetch(query)
+            .then((data) => setAbouts(data))
+    }, []);
 
     return (
         <>
@@ -34,7 +20,7 @@ const About = () => {
             </h2>
 
             <div className="app__profiles">
-                {about.map((about, index) => (
+                {abouts.map((about, index) => (
                     <motion.div
                         whileInView={{ opacity: 1 }}
                         whileHover={{ scale: 1.1 }}
@@ -42,7 +28,7 @@ const About = () => {
                         className="app__profile-item"
                         key={about.title + index}
                     >
-                        <img src={about.imgUrl} alt={about.title} />
+                        <img src={urlFor(about.imgUrl)} alt={about.title} />
                         <h2 className="bold-text" style={{margin: 20}}>
                             {about.title}
                         </h2>
