557e8996d6dfb5f98ff578fa1555351fd7db88ba
Gregdabro <gregmonak@gmail.com>
2022-08-11 14:50:02 +0200

    implement skills section

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 import "./Work.scss";
 
@@ -19,8 +19,6 @@ const Work = () => {
                 setFilterWork(data);
             })
     }, [])
-
-    console.log(filterWork)
 
     const handleWorkFilter = (item) => {
         setActiveFilter(item);
@@ -107,4 +105,8 @@ const Work = () => {
     );
 };
 
-export default AppWrap(Work, "work");
+export default AppWrap(
+    MotionWrap(Work, "app__works"),
+    "work",
+    "app__primarybg"
+);
